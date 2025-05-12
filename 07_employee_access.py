123c102f259365ba77d1cccd246b5ef9c491f880
Muzamil Ahmed <159611148+Amuzammill@users.noreply.github.com>
2025-05-12 13:07:38 +0500

    Update 07_employee_access.py

--- 07_employee_access.py
+++ 07_employee_access.py
@@ -5,7 +5,7 @@ class Employee:
         self.__ssn = ssn          # private
 
 # Test
-emp = Employee("John", 50000, "123-45-6789")
+emp = Employee("Muzamil", 50000, "123-45-6789")
 print(emp.name)           # Accessible
 print(emp._salary)        # Accessible but discouraged
 # print(emp.__ssn)        # Will raise AttributeError
